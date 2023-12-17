059931dc1d684c6de1b51279b47c580ab674524d
NLopezT04 <Nestor.Lopez2@alu.uclm.es>
2023-12-17 14:07:39 +0100

    Update PortFolio

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'App Developer'],
+    strings: ['Frontend Developer', 'App Developer', 'Student'],
     typeSpeed: 100, 
     backSpeed: 100,
     backDelay: 1000,
